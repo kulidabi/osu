441324b72a39ac5799ebafc7d419a67ac9d211c1
kulidabi <kulidabi@gmail.com>
2025-05-26 18:37:43 +0200

    Update InspectCode.sh

--- InspectCode.sh
+++ InspectCode.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 dotnet tool restore
 dotnet CodeFileSanity
 dotnet jb inspectcode "osu.Desktop.slnf" --no-build --output="inspectcodereport.xml" --caches-home="inspectcode" --verbosity=WARN
